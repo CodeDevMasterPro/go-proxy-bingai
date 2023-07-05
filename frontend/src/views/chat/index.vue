c5474292be7430d9c646adab8390287e7d66835d
CodeDevMaster <41275512+CodeDevMasterPro@users.noreply.github.com>
2023-07-05 09:23:32 +0800

    Update index.vue
    删除ChatNav

--- frontend/src/views/chat/index.vue
+++ frontend/src/views/chat/index.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import ChatNav from '@/components/ChatNav/ChatNav.vue';
+// import ChatNav from '@/components/ChatNav/ChatNav.vue';
 import ChatPromptStore from '@/components/ChatPromptStore/ChatPromptStore.vue';
 import Chat from './components/Chat/Chat.vue';
 </script>
 
 <template>
   <main>
-    <ChatNav />
     <ChatPromptStore />
     <Chat />
   </main>
